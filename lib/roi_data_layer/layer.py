4eee366a19a219543324f1d6632c4b7d463bb15b
Huan <tonihs@gmail.com>
2017-01-18 00:28:24 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed: 	new file:   README.md 	new file:   lib/Makefile 	new file:   lib/_init_paths.py 	new file:   lib/blob.py 	new file:   lib/config.py 	new file:   lib/datalayer.py 	new file:   lib/datasets/VOCdevkit-matlab-wrapper/get_voc_opts.m 	new file:   lib/datasets/VOCdevkit-matlab-wrapper/voc_eval.m 	new file:   lib/datasets/VOCdevkit-matlab-wrapper/xVOCap.m 	new file:   lib/datasets/__init__.py 	new file:   lib/datasets/coco.py 	new file:   lib/datasets/ds_utils.py 	new file:   lib/datasets/factory.py 	new file:   lib/datasets/imdb.py 	new file:   lib/datasets/pascal_voc.py 	new file:   lib/datasets/psdb.py 	new file:   lib/datasets/tools/mcg_munge.py 	new file:   lib/datasets/voc_eval.py 	new file:   lib/fast_rcnn/__init__.py 	new file:   lib/fast_rcnn/bbox_transform.py 	new file:   lib/fast_rcnn/config.py 	new file:   lib/fast_rcnn/nms_wrapper.py 	new file:   lib/fast_rcnn/test.py 	new file:   lib/fast_rcnn/test_gallery.py 	new file:   lib/fast_rcnn/test_probe.py 	new file:   lib/fast_rcnn/train.py 	new file:   lib/nms/.gitignore         new file:   lib/nms/__init__.py 	new file:   lib/nms/cpu_nms.pyx 	new file:   lib/nms/gpu_nms.hpp 	new file:   lib/nms/gpu_nms.pyx 	new file:   lib/nms/nms_kernel.cu 	new file:   lib/nms/py_cpu_nms.py 	new file:   lib/norm2layer.py 	new file:   lib/norm2layer128.py 	new file:   lib/norm2layer300.py 	new file:   lib/pycocotools/UPSTREAM_REV 	new file:   lib/pycocotools/__init__.py 	new file:   lib/pycocotools/_mask.c 	new file:   lib/pycocotools/_mask.pyx 	new file:   lib/pycocotools/coco.py 	new file:   lib/pycocotools/cocoeval.py 	new file:   lib/pycocotools/license.txt 	new file:   lib/pycocotools/mask.py 	new file:   lib/pycocotools/maskApi.c 	new file:   lib/pycocotools/maskApi.h 	new file:   lib/roi_data_layer/__init__.py 	new file:   lib/roi_data_layer/layer.py 	new file:   lib/roi_data_layer/layer_ori.py 	new file:   lib/roi_data_layer/minibatch.py 	new file:   lib/roi_data_layer/minibatch_ori.py 	new file:   lib/roi_data_layer/pair_layer.py 	new file:   lib/roi_data_layer/pair_minibatch.py 	new file:   lib/roi_data_layer/roidb.py 	new file:   lib/rpn/README.md 	new file:   lib/rpn/__init__.py 	new file:   lib/rpn/anchor_target_layer.py 	new file:   lib/rpn/anchor_target_layer_ori.py 	new file:   lib/rpn/generate.py 	new file:   lib/rpn/generate_anchors.py 	new file:   lib/rpn/proposal_layer.py 	new file:   lib/rpn/proposal_layer_ori.py 	new file:   lib/rpn/proposal_target_layer.py 	new file:   lib/rpn/proposal_target_layer_ori.py 	new file:   lib/sampledata.py 	new file:   lib/setup.py 	new file:   lib/timer.py 	new file:   lib/train.py 	new file:   lib/transform/__init__.py 	new file:   lib/transform/torch_image_transform_layer.py 	new file:   lib/tripletloss/_init_paths.py 	new file:   lib/tripletloss/blob.py 	new file:   lib/tripletloss/config.py 	new file:   lib/tripletloss/datalayer.py 	new file:   lib/tripletloss/norm2layer.py 	new file:   lib/tripletloss/sampledata.py 	new file:   lib/tripletloss/timer.py 	new file:   lib/tripletloss/train.py 	new file:   lib/tripletloss/tripletlosslayer.py 	new file:   lib/tripletloss/tripletselectlayer.py 	new file:   lib/tripletlosslayer.py 	new file:   lib/tripletselectlayer.py 	new file:   lib/utils/.gitignore 	new file:   lib/utils/__init__.py 	new file:   lib/utils/bbox.pyx 	new file:   lib/utils/blob.py         new file:   lib/utils/timer.py

---
+++ lib/roi_data_layer/layer.py
@@ -0,0 +1,258 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""The data layer used during training to train a Fast R-CNN network.
+
+RoIDataLayer implements a Caffe Python layer.
+"""
+
+import caffe
+from fast_rcnn.config import cfg
+from roi_data_layer.minibatch import get_minibatch
+import numpy as np
+import yaml
+from multiprocessing import Process, Queue
+import pdb
+
+class RoIDataLayer(caffe.Layer):
+    """Fast R-CNN data layer used for training."""
+
+    def _shuffle_roidb_inds(self):
+        """Randomly permute the training roidb."""
+        if cfg.TRAIN.ASPECT_GROUPING:
+            widths = np.array([r['width'] for r in self._roidb])
+            heights = np.array([r['height'] for r in self._roidb])
+            horz = (widths >= heights)
+            vert = np.logical_not(horz)
+            horz_inds = np.where(horz)[0]
+            vert_inds = np.where(vert)[0]
+            inds = np.hstack((
+                np.random.permutation(horz_inds),
+                np.random.permutation(vert_inds)))
+            inds = np.reshape(inds, (-1, 2))
+            row_perm = np.random.permutation(np.arange(inds.shape[0]))
+            inds = np.reshape(inds[row_perm, :], (-1,))
+            self._perm = inds
+        else:
+            self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    #Huan add
+    def _get_triplet_inds(self):
+        """Return triplet set inds"""      
+        anchor_inds = 0
+        positive_inds = 0
+
+        getAnchor = 0
+        getPositive = 0
+
+        anchorPid = 0
+        positivePid = 0
+
+        #lets find an anchor that does not have all gt_pids == [-1,-1... -1]
+        for indexAnchordata in range(self._cur,len(self._roidb)):
+            if getAnchor:
+               anchor_inds = indexAnchordata-1
+               break #jump out anchor data loop
+            for indexPid in range(len(self._roidb[self._perm[indexAnchordata]]['gt_pids'])):
+                anchorPid =  self._roidb[self._perm[indexAnchordata]]['gt_pids'][indexPid] 
+	        if anchorPid < 0:
+                   continue
+                else:
+                   anchor = self._roidb[self._perm[indexAnchordata]] 
+                   getAnchor = 1
+                   break #jump out anchor pids loop
+         
+        #if got an anchor, then lets get positive sample
+        for indexPosdata in range(0,len(self._roidb)):
+            if getPositive:
+               positive_inds = indexPosdata-1
+               break #jump out positive data loop
+            elif indexPosdata == anchor_inds:
+               continue #because searching from 0 to end, do not return anchor again
+
+            for indexPid in range(len(self._roidb[self._perm[indexPosdata]]['gt_pids'])):
+                positivePid =  self._roidb[self._perm[indexPosdata]]['gt_pids'][indexPid] 
+	        if positivePid != anchorPid:
+                   continue
+                else:
+                   positive = self._roidb[self._perm[indexPosdata]] 
+                   getPositive = 1
+                   break #jump out positive pids loop
+
+        return anchor_inds, positive_inds, getPositive
+
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds()
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+
+        #triplet loss
+        while self._cur<len(self._roidb):
+              anchor_inds, positive_inds, getPositive= self._get_triplet_inds()
+              if not getPositive: #do not have matching positive for anchor
+                     self._cur += 1
+              else:
+                  db_inds[0] = self._perm[anchor_inds]
+                  db_inds[1] = self._perm[positive_inds]
+                  self._cur = anchor_inds
+                  break
+        self._cur += 1 #go to next anchor
+        return db_inds
+
+    def _get_next_minibatch(self):
+        """Return the blobs to be used for the next minibatch.
+
+        If cfg.TRAIN.USE_PREFETCH is True, then blobs will be computed in a
+        separate process and made available through self._blob_queue.
+        """
+        if cfg.TRAIN.USE_PREFETCH:
+            return self._blob_queue.get()
+        else:
+            #pdb.set_trace()
+            db_inds = self._get_next_minibatch_inds()
+            minibatch_db = [self._roidb[i] for i in db_inds]
+            return get_minibatch(minibatch_db, self._num_classes)
+
+    def set_roidb(self, roidb):
+        """Set the roidb to be used by this layer during training."""
+        self._roidb = roidb
+        self._shuffle_roidb_inds()
+        if cfg.TRAIN.USE_PREFETCH:
+            self._blob_queue = Queue(10)
+            self._prefetch_process = BlobFetcher(self._blob_queue,
+                                                 self._roidb,
+                                                 self._num_classes)
+            self._prefetch_process.start()
+            # Terminate the child process when the parent exists
+            def cleanup():
+                print 'Terminating BlobFetcher'
+                self._prefetch_process.terminate()
+                self._prefetch_process.join()
+            import atexit
+            atexit.register(cleanup)
+
+    def setup(self, bottom, top):
+        """Setup the RoIDataLayer."""
+
+        # parse the layer parameter string, which must be valid YAML
+        layer_params = yaml.load(self.param_str_)
+
+        self._num_classes = layer_params['num_classes']
+
+        self._name_to_top_map = {}
+
+        # data blob: holds a batch of N images, each with 3 channels
+        idx = 0
+        top[idx].reshape(cfg.TRAIN.IMS_PER_BATCH, 3,
+            max(cfg.TRAIN.SCALES), cfg.TRAIN.MAX_SIZE)
+        self._name_to_top_map['data'] = idx
+        idx += 1
+
+        if cfg.TRAIN.HAS_RPN:
+            top[idx].reshape(1, 3)
+            self._name_to_top_map['im_info'] = idx
+            idx += 1
+
+            top[idx].reshape(1, 4)
+            self._name_to_top_map['gt_boxes'] = idx
+            idx += 1
+
+            #add for IMS.BATCH.NUMBER
+            top[idx].reshape(1)
+            self._name_to_top_map['im_inds'] = idx
+            idx += 1
+        else: # not using RPN
+            # rois blob: holds R regions of interest, each is a 5-tuple
+            # (n, x1, y1, x2, y2) specifying an image batch index n and a
+            # rectangle (x1, y1, x2, y2)
+            top[idx].reshape(1, 5)
+            self._name_to_top_map['rois'] = idx
+            idx += 1
+
+            # labels blob: R categorical labels in [0, ..., K] for K foreground
+            # classes plus background
+            top[idx].reshape(1)
+            self._name_to_top_map['labels'] = idx
+            idx += 1
+
+            if cfg.TRAIN.BBOX_REG:
+                # bbox_targets blob: R bounding-box regression targets with 4
+                # targets per class
+                top[idx].reshape(1, self._num_classes * 4)
+                self._name_to_top_map['bbox_targets'] = idx
+                idx += 1
+
+                # bbox_inside_weights blob: At most 4 targets per roi are active;
+                # thisbinary vector sepcifies the subset of active targets
+                top[idx].reshape(1, self._num_classes * 4)
+                self._name_to_top_map['bbox_inside_weights'] = idx
+                idx += 1
+
+                top[idx].reshape(1, self._num_classes * 4)
+                self._name_to_top_map['bbox_outside_weights'] = idx
+                idx += 1
+
+        print 'RoiDataLayer: name_to_top:', self._name_to_top_map
+        assert len(top) == len(self._name_to_top_map)
+
+    def forward(self, bottom, top):
+        """Get blobs and copy them into this layer's top blob vector."""
+        blobs = self._get_next_minibatch()
+        for blob_name, blob in blobs.iteritems():
+            top_ind = self._name_to_top_map[blob_name]
+            # Reshape net's input blobs
+            top[top_ind].reshape(*(blob.shape))
+            # Copy data into net's input blobs
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
+
+class BlobFetcher(Process):
+    """Experimental class for prefetching blobs in a separate process."""
+    def __init__(self, queue, roidb, num_classes):
+        super(BlobFetcher, self).__init__()
+        self._queue = queue
+        self._roidb = roidb
+        self._num_classes = num_classes
+        self._perm = None
+        self._cur = 0
+        self._shuffle_roidb_inds()
+        # fix the random seed for reproducibility
+        np.random.seed(cfg.RNG_SEED)
+
+    def _shuffle_roidb_inds(self):
+        """Randomly permute the training roidb."""
+        # TODO(rbg): remove duplicated code
+        self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        # TODO(rbg): remove duplicated code
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds()
+
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+        self._cur += cfg.TRAIN.IMS_PER_BATCH
+        return db_inds
+
+    def run(self):
+        print 'BlobFetcher started'
+        while True:
+            db_inds = self._get_next_minibatch_inds()
+            minibatch_db = [self._roidb[i] for i in db_inds]
+            blobs = get_minibatch(minibatch_db, self._num_classes)
+            self._queue.put(blobs)
---
+++ lib/rpn/proposal_target_layer.py
@@ -0,0 +1,220 @@
+# --------------------------------------------------------
+# Faster R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick and Sean Bell
+# --------------------------------------------------------
+
+import caffe
+import yaml
+import numpy as np
+import numpy.random as npr
+from fast_rcnn.config import cfg
+from fast_rcnn.bbox_transform import bbox_transform
+from utils.cython_bbox import bbox_overlaps
+import pdb
+
+DEBUG = False
+
+class ProposalTargetLayer(caffe.Layer):
+    """
+    Assign object detection proposals to ground-truth targets. Produces proposal
+    classification labels and bounding-box regression targets.
+    """
+
+    def setup(self, bottom, top):
+        layer_params = yaml.load(self.param_str_)
+        self._num_classes = layer_params['num_classes']
+
+	ims_per_batch = cfg.TRAIN.IMS_PER_BATCH;
+
+        # sampled rois (0, x1, y1, x2, y2)
+        top[0].reshape(1, 5)
+        # labels
+        top[1].reshape(1, 1)
+        # bbox_targets
+        top[2].reshape(1,self._num_classes * 4)
+        # bbox_inside_weights
+        top[3].reshape(1, self._num_classes * 4)
+        # bbox_outside_weights
+        top[4].reshape(1, self._num_classes * 4)
+
+    def forward(self, bottom, top):
+        # Proposal ROIs (0, x1, y1, x2, y2) coming from RPN
+        # (i.e., rpn.proposal_layer.ProposalLayer), or any other source
+        all_rois = bottom[0].data
+        # GT boxes (x1, y1, x2, y2, label)
+        # TODO(rbg): it's annoying that sometimes I have extra info before
+        # and other times after box coordinates -- normalize to one format
+        gt_boxes = bottom[1].data
+	im_inds = bottom[2].data
+
+        # Include ground-truth boxes in the set of candidate rois
+        # zeros = np.zeros((gt_boxes.shape[0], 1), dtype=gt_boxes.dtype)
+        all_rois = np.vstack(
+            (all_rois, np.hstack((im_inds, gt_boxes[:, :-1])))
+        )
+
+        # Sanity check: single batch only
+        #assert np.all(all_rois[:, 0] == 0), \
+        #        'Only single item batches are supported'
+	# num_images = 1;
+
+        num_images = cfg.TRAIN.IMS_PER_BATCH;
+        rois_per_image = cfg.TRAIN.BATCH_SIZE;
+        #rois_per_image = cfg.TRAIN.BATCH_SIZE / num_images
+        fg_rois_per_image = int(np.round(cfg.TRAIN.FG_FRACTION * rois_per_image))
+
+        # Sample rois with classification labels and bounding box regression
+        # targets
+	labels_all = []; rois_all = []; bbox_targets_all = []; bbox_inside_weights_all = [];
+	for im_i in range(num_images):
+		inds_i = np.where(all_rois[:,0] == im_i)[0];
+		all_rois_i = all_rois[inds_i,:];
+		inds_i = np.where(im_inds == im_i)[0];
+		gt_boxes_i = gt_boxes[inds_i,:];
+
+	        labels, rois, bbox_targets, bbox_inside_weights = _sample_rois(
+	            all_rois_i, gt_boxes_i, fg_rois_per_image,
+	            rois_per_image, self._num_classes)
+		if labels_all == [] and labels != []:
+			labels = labels.reshape(len(labels),1);
+			labels_all = labels;
+			rois_all = rois;
+			bbox_targets_all = bbox_targets;
+			bbox_inside_weights_all = bbox_inside_weights;
+		else:
+			if labels != []:
+				labels = labels.reshape(len(labels),1);
+				labels_all = np.vstack((labels_all,labels));
+				rois_all = np.vstack((rois_all,rois));
+				bbox_targets_all = np.vstack((bbox_targets_all,bbox_targets));
+				bbox_inside_weights_all = np.vstack((bbox_inside_weights_all,bbox_inside_weights));
+
+        if DEBUG:
+            print 'num fg: {}'.format((labels > 0).sum())
+            print 'num bg: {}'.format((labels == 0).sum())
+            self._count += 1
+            self._fg_num += (labels > 0).sum()
+            self._bg_num += (labels == 0).sum()
+            print 'num fg avg: {}'.format(self._fg_num / self._count)
+            print 'num bg avg: {}'.format(self._bg_num / self._count)
+            print 'ratio: {:.3f}'.format(float(self._fg_num) / float(self._bg_num))
+	#pdb.set_trace();
+        # sampled rois
+        top[0].reshape(*rois_all.shape)
+        top[0].data[...] = rois_all
+
+        # classification labels
+	#labels_all = labels_all.reshape(labels_all.shape[0]*labels_all.shape[1]);
+        top[1].reshape(*labels_all.shape)
+        top[1].data[...] = labels_all
+
+        # bbox_targets
+        top[2].reshape(*bbox_targets_all.shape)
+        top[2].data[...] = bbox_targets_all
+
+        # bbox_inside_weights
+        top[3].reshape(*bbox_inside_weights_all.shape)
+        top[3].data[...] = bbox_inside_weights_all
+
+        # bbox_outside_weights
+        top[4].reshape(*bbox_inside_weights_all.shape)
+        top[4].data[...] = np.array(bbox_inside_weights_all > 0).astype(np.float32)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
+
+
+def _get_bbox_regression_labels(bbox_target_data, num_classes):
+    """Bounding-box regression targets (bbox_target_data) are stored in a
+    compact form N x (class, tx, ty, tw, th)
+
+    This function expands those targets into the 4-of-4*K representation used
+    by the network (i.e. only one class has non-zero targets).
+
+    Returns:
+        bbox_target (ndarray): N x 4K blob of regression targets
+        bbox_inside_weights (ndarray): N x 4K blob of loss weights
+    """
+
+    clss = bbox_target_data[:, 0]
+    bbox_targets = np.zeros((clss.size, 4 * num_classes), dtype=np.float32)
+    bbox_inside_weights = np.zeros(bbox_targets.shape, dtype=np.float32)
+    inds = np.where(clss > 0)[0]
+    for ind in inds:
+        cls = clss[ind]
+        start = 4 * cls
+        end = start + 4
+        bbox_targets[ind, start:end] = bbox_target_data[ind, 1:]
+        bbox_inside_weights[ind, start:end] = cfg.TRAIN.BBOX_INSIDE_WEIGHTS
+    return bbox_targets, bbox_inside_weights
+
+
+def _compute_targets(ex_rois, gt_rois, labels):
+    """Compute bounding-box regression targets for an image."""
+
+    assert ex_rois.shape[0] == gt_rois.shape[0]
+    assert ex_rois.shape[1] == 4
+    assert gt_rois.shape[1] == 4
+
+    targets = bbox_transform(ex_rois, gt_rois)
+    if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
+        # Optionally normalize targets by a precomputed mean and stdev
+        targets = ((targets - np.array(cfg.TRAIN.BBOX_NORMALIZE_MEANS))
+                / np.array(cfg.TRAIN.BBOX_NORMALIZE_STDS))
+    return np.hstack(
+            (labels[:, np.newaxis], targets)).astype(np.float32, copy=False)
+
+def _sample_rois(all_rois, gt_boxes, fg_rois_per_image, rois_per_image, num_classes):
+    """Generate a random sample of RoIs comprising foreground and background
+    examples.
+    """
+    # overlaps: (rois x gt_boxes)
+    overlaps = bbox_overlaps(
+        np.ascontiguousarray(all_rois[:, 1:5], dtype=np.float),
+        np.ascontiguousarray(gt_boxes[:, :4], dtype=np.float))
+    gt_assignment = overlaps.argmax(axis=1)
+    max_overlaps = overlaps.max(axis=1)
+    labels = gt_boxes[gt_assignment, 4]
+
+    # Select foreground RoIs as those with >= FG_THRESH overlap
+    fg_inds = np.where(max_overlaps >= cfg.TRAIN.FG_THRESH)[0]
+    # Guard against the case when an image has fewer than fg_rois_per_image
+    # foreground RoIs
+    fg_rois_per_this_image = min(fg_rois_per_image, fg_inds.size)
+    # Sample foreground regions without replacement
+    if fg_inds.size > 0:
+        fg_inds = npr.choice(fg_inds, size=fg_rois_per_this_image, replace=False)
+
+    # Select background RoIs as those within [BG_THRESH_LO, BG_THRESH_HI)
+    bg_inds = np.where((max_overlaps < cfg.TRAIN.BG_THRESH_HI) &
+                       (max_overlaps >= cfg.TRAIN.BG_THRESH_LO))[0]
+    # Compute number of background RoIs to take from this image (guarding
+    # against there being fewer than desired)
+    bg_rois_per_this_image = rois_per_image - fg_rois_per_this_image
+    bg_rois_per_this_image = min(bg_rois_per_this_image, bg_inds.size)
+    # Sample background regions without replacement
+    if bg_inds.size > 0:
+        bg_inds = npr.choice(bg_inds, size=bg_rois_per_this_image, replace=False)
+
+    # The indices that we're selecting (both fg and bg)
+    keep_inds = np.append(fg_inds, bg_inds)
+    # Select sampled values from various arrays:
+    labels = labels[keep_inds]
+    # Clamp labels for the background RoIs to 0
+    labels[fg_rois_per_this_image:] = 0
+    rois = all_rois[keep_inds]
+
+    bbox_target_data = _compute_targets(
+        rois[:, 1:5], gt_boxes[gt_assignment[keep_inds], :4], labels)
+
+    bbox_targets, bbox_inside_weights = \
+        _get_bbox_regression_labels(bbox_target_data, num_classes)
+
+    return labels, rois, bbox_targets, bbox_inside_weights
