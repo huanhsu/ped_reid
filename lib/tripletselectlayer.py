4ae968ea9f1d53309c958d0be6872e8da453ef16
Huan <tonihs@gmail.com>
2017-02-04 22:07:26 +0800

    process cases of all -1 or 5532

--- lib/tripletselectlayer.py
+++ lib/tripletselectlayer.py
@@ -39,32 +39,52 @@ class TripletSelectLayer(caffe.Layer):
         aps = {}
         ans = {}
         #collect triplet set
-        imgs_idx = shape(bottom[1].data)[0] #2
-        feats_idx = shape(bottom[1].data)[1] #128
+        imgs_idx = shape(bottom[1].data)[0] #3
+        feats_idx = shape(bottom[1].data)[1] #64
+
         #archor, take from bottom[1].data[0]
-        archor_list = np.where(bottom[1].data[0][0:feats_idx-1]!=5532)
+        archor_list = np.intersect1d(np.where(bottom[1].data[0][0:feats_idx-1]!=5532), np.where(bottom[1].data[0][0:feats_idx-1]!=-1))
+        if len(archor_list) == 0:
+            archor_list = np.where(bottom[1].data[0][0:feats_idx-1]==5532)
+            print('*** background as anchor ***')
+        else:
+            archor_list = np.where(bottom[1].data[0][0:feats_idx-1]!=5532)
+
+        count = 0
         while True:
               archor_index = randrange(0,shape(archor_list)[1])
               archor_index = archor_list[0][archor_index]
               archor_value = bottom[1].data[0][archor_index]
+              if archor_value<0 and count < 1000:
+                 count += 1
+                 continue
               pos_list_temp = np.where(bottom[1].data[1][0:feats_idx-1]==archor_value)
               if shape(pos_list_temp)[1]>0:
                  break
+
         #positive, take from bottom[1].data[1]
         positive_list = np.where(bottom[1].data[1][0:feats_idx-1]==archor_value)
         positive_index = randrange(0, shape(positive_list)[1])
         positive_index = positive_list[0][positive_index]
         positive_value = bottom[1].data[1][positive_index]
-        #negitive, take from bottom[1].data[1]
-        negitive_list = np.where(bottom[1].data[1][0:feats_idx-1]!=archor_value)
-        negitive_index = randrange(0, shape(negitive_list)[1])
-        negitive_index = negitive_list[0][negitive_index]
-        negitive_value = bottom[1].data[1][negitive_index]
+        
+        #negative, take from bottom[1].data[2]
+        negative_list = np.intersect1d(np.where(bottom[1].data[2][0:feats_idx-1]!=5532), np.where(bottom[1].data[2][0:feats_idx-1]!=-1))
+        if len(negative_list) == 0:
+           negative_list = np.where(bottom[1].data[2][0:feats_idx-1]==5532)
+           print ('*** background as negative ***')
+           negative_index = randrange(0, shape(negative_list)[0])
+           negative_index = negative_list[0][negative_index]
+        else:
+           negative_index = randrange(0, shape(negative_list)[0])
+           negative_index = negative_list[negative_index]
+        negative_value = bottom[1].data[2][negative_index]
 
+        print ('Triplet set'+' an:'+str(archor_value)+' '+'pos:'+str(positive_value)+' '+'neg:'+str(negative_value))
         #index in feature
         archor_feature_index = archor_index
-        positive_feature_index = positive_index + 128
-        negitive_feature_index = negitive_index + 128 
+        positive_feature_index = positive_index + feats_idx
+        negative_feature_index = negative_index + feats_idx*2 
 
         #collect triplet set old
         archor_feature = bottom[0].data[archor_feature_index]
@@ -75,7 +95,7 @@ class TripletSelectLayer(caffe.Layer):
             aps[i+self.triplet] = ap
         aps = sorted(aps.items(), key = lambda d: d[1], reverse = True)
         for i in range(self.triplet):
-            negative_feature = bottom[0].data[negitive_feature_index]
+            negative_feature = bottom[0].data[negative_feature_index]
             a_n = archor_feature - negative_feature
             an = np.dot(a_n,a_n)
             ans[i+self.triplet*2] = an
@@ -84,10 +104,10 @@ class TripletSelectLayer(caffe.Layer):
         for i in range(self.triplet): 
             top_archor.append(bottom[0].data[archor_feature_index])
             top_positive.append(bottom[0].data[positive_feature_index])
-            top_negative.append(bottom[0].data[negitive_feature_index])
+            top_negative.append(bottom[0].data[negative_feature_index])
             if aps[i][1] >= ans[i][1]:
                self.no_residual_list.append(i)
-            self.tripletlist.append([archor_feature_index,positive_feature_index,negitive_feature_index])
+            self.tripletlist.append([archor_feature_index,positive_feature_index,negative_feature_index])
 
         top[0].data[...] = np.array(top_archor).astype(float32)
         top[1].data[...] = np.array(top_positive).astype(float32)
